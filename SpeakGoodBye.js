4610bc77327797e446a66b00dafc3158418ee606
satyendra130 <65526293+satyendra130@users.noreply.github.com>
2020-06-08 13:50:06 +0530

    Delete SpeakGoodBye.js

--- SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-    var byeSpeaker={};
-    var speak="Good Bye";
-    byeSpeaker.sayBye=function(name){
-        console.log(speak+" "+name);
-    }
-    window.byeSpeaker=byeSpeaker;
-
-})(window);
