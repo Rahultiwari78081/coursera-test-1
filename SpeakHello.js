6205fdff716ca39452e38e4b2feb5871afbc6da5
satyendra130 <65526293+satyendra130@users.noreply.github.com>
2020-06-08 13:51:08 +0530

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-    var helloSpeaker={};
-    helloSpeakerName="hello";
-    helloSpeaker.sayhello=function(name){
-        console.log(helloSpeakerName+" "+name);
-    }
-    window.helloSpeaker=helloSpeaker;
-
-})(window);
